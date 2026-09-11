5f7257a2db220c9887d2c0b90b12385a61db77c6
renderprueba09-prog <renderprueba09@gmail.com>
2026-09-11 16:31:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -408,7 +408,6 @@ def registrar_entrega(entrega_id):
 def logout():
     session.clear()
     return redirect(url_for('login'))
-
 if __name__ == '__main__':
-    init_db()
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
